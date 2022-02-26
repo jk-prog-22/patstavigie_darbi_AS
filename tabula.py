1062ae8d03d729d82be0433efb19b53bce99b1df
artursSpirands <100462614+Snake552@users.noreply.github.com>
2022-02-26 14:33:10 +0200

    Add files via upload

---
+++ tabula.py
@@ -0,0 +1,13 @@
+# 1.uzd
+vards = str(input("Ievadi savu vārdu"))
+
+if vards == "Bond":
+    print("Esi sveicināts 007")
+elif vards != "Bond": 
+    print("Esi sveicināts!",vards)
+# 2.uzd
+skaitlis = int(input("Ievadi skaitli: "))
+if skaitlis (2 == 0):
+    print("Skaitlis", skaitlis, "ir pāra, dalās ar 2")
+else: 
+    print("Skaitlis", skaitlis, "ir nepāra, nedalās ar 2")

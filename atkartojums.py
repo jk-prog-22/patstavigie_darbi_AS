9cdcbe22b15f07e433402d4e86499ed17fe20b7b
artursSpirands <100462614+Snake552@users.noreply.github.com>
2022-03-12 09:45:40 +0200

    Add files via upload

---
+++ atkartojums.py
@@ -0,0 +1,27 @@
+Menesis = input("Ievadi menesa nosaukumu: ")
+if Menesis == "janvaris":
+    print("Šajā mēnesī ir 31 diena")
+elif Menesis == "Februaris": 
+    print("Šajā mēnesī šoreiz ir 28 dienas(2022)")
+elif Menesis == "Marts": 
+    print("Šajā mēnesī ir 31 diena")
+elif Menesis == "Aprilis": 
+    print("Šajā mēnesī ir 30 dienas")
+elif Menesis == "Maijs": 
+    print("Šajā mēnesī ir 31 diena")
+elif Menesis == "Junijs": 
+    print("Šajā mēnesī ir 30 dienas")
+elif Menesis == "Julijs": 
+    print("Šajā mēnesī ir 31 diena")
+elif Menesis == "Augusts": 
+    print("Šajā mēnesī ir 31 diena")
+elif Menesis == "Septembris": 
+    print("Šajā mēnesī ir 30 dienas")
+elif Menesis == "Oktobris": 
+    print("Šajā mēnesī ir 31 diena")
+elif Menesis == "Novembris": 
+    print("Šajā mēnesī ir 30 dienas")
+elif Menesis == "Decembris": 
+    print("Šajā mēnesī ir 31 diena")
+
+

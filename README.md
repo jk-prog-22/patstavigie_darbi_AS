7807d969db46d6bb0845f5f9d9d950379bc1818f
artursSpirands <arcijs886@gmail.com>
2022-03-12 09:05:39 +0200

    2.uzd

---
+++ README.md
@@ -0,0 +1,15 @@
+# Mans pirmais repozitorijs
+
+Šodien ievads par versiju konroles sistēmu. 
+
+## Svarīgākās komandas
+``` bash
+git init
+git status
+git commit -m "ziņa"
+git add. 
+```
+# 3.uzd
+vertejums = int(input("ievadi vērtējumu: "))
+if vertejums <= 3:
+    print("Nekas labs tev jau nav")
---
+++ tabula.py
@@ -0,0 +1,29 @@
+# 1.uzd
+vards = str(input("Ievadi savu vārdu"))
+
+if vards == "Bond":
+    print("Esi sveicināts 007")
+elif vards != "Bond": 
+    print("Esi sveicināts!",vards)
+
+
+
+# 2.uzd
+skaitlis = int(input("Ievadi skaitli: "))
+print("atlikums", skaitlis % 2)
+if (skaitlis % 2) == 0:
+    print("Skaitlis", skaitlis,"ir pāra")
+elif (skaitlis % 2) != 0:
+    print("Skaitlis", skaitlis,"ir nepāra")
+
+
+
+
+
+
+
+
+
+
+
+    
